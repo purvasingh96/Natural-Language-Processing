8c9e304a677615cf7c879f46548452cb848a5da3
purva <singhpurva2906@gmail.com>
2019-04-07 16:25:55 +0530

    master: chinking examples.

--- 5. Chunking.py
+++ 5. Chunking.py
@@ -15,7 +15,8 @@ def process_content():
         for i in tokenized:
             words = nltk.word_tokenize(i)
             tagged = nltk.pos_tag(words)
-            chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?}"""
+            chunkGram = r"""Chunk: {<.*>+}
+                                    }<VB.?|IN|DT|TO>+{"""
             chunkParser = nltk.RegexpParser(chunkGram)
             chunked = chunkParser.parse(tagged)
 
