aa20408668be9e7083c2ea1cd5b4a2a1e482b00f
Purva Singh <singhpurva2906@gmail.com>
2019-04-09 05:50:20 +0000

    Create 1. twitter_api_basics.py

---
+++ sentiment-analysis/1. twitter_api_basics.py
@@ -0,0 +1,28 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+
+ckey = '****'
+csecret = '****'
+atoken = '**** '
+asecret = '****'
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+
+
+    def on_error(self, status):
+        print(status)
+        return status
+
+# authorizing user
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["car"])
+
+#https://www.digitalocean.com/community/tutorials/how-to-set-up-time-synchronization-on-ubuntu-16-04
