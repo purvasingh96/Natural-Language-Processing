c9bd0b2d02bdced1450afbeb132c0b25a539f1e5
purva <singhpurva2906@gmail.com>
2019-04-09 08:47:55 +0530

    master: text classification examples.

--- 10. Text Classification.py
+++ 10. Text Classification.py
@@ -13,5 +13,22 @@ all_words = []
 for w in movie_reviews.words():
     all_words.append(w.lower())
 
+# order from most common to least common words
 all_words = nltk.FreqDist(all_words)
 print(all_words)
+
+# list of most common 3000 words
+word_features = list(all_words.keys())[:3000]
+
+# this function will search if most common words
+# is present in document as well
+# then append it to features list
+
+def find_features(document):
+    words = set(document)
+    features={}
+    for w in word_features:
+        features[w]=(w in words)
+    return features
+
+print(find_features(movie_reviews.words('neg/cv000_29416.txt')))
